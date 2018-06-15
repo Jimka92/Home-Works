01f2dbada72210af12c8d5753289977dd5ae26a8
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-06-15 16:28:48 +0400

    Add files via upload

---
+++ event-bubbling-capturing/shop/js/cart.js
@@ -0,0 +1,10 @@
+'use strict';
+
+list.addEventListener('click', checkCart);
+
+function checkCart(event) {
+	if ((event.target.tagName === 'A') && (event.target.classList.contains('add-to-cart'))) {
+		event.preventDefault();
+		addToCart(event.target.dataset);	
+	};	
+}
