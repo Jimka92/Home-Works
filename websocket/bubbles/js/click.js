fcb58082fd2d59df3723ac94297a545b0c4abde2
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-06-20 14:46:13 +0400

    Add files via upload

---
+++ websocket/bubbles/js/click.js
@@ -0,0 +1,15 @@
+'use strinct';
+
+const body = document.querySelector('body')
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', () => {
+	showBubbles(connection);
+});
+
+body.addEventListener('click', event => {
+	connection.send(JSON.stringify({ 
+		x: event.x,
+		y: event.y
+	}));
+})
---
+++ websocket/counter/js/counter.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const outputCounter = document.querySelector('.counter');
+const outputErrors = document.querySelector('.errors');
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connection.addEventListener('message', event => {
+	let message = JSON.parse(event.data);
+	outputCounter.innerText = `${message.connections}`;
+	outputErrors.value = `${message.errors}`;
+});
+
+window.addEventListener('beforeunload', () => {
+	connection.close(1000)
+})
